cd0b3f21d74015f19f90b9936f4c332f0d8dda38
keti <qeti.tsiklauri320@ens.tsu.edu.ge>
2025-10-01 16:05:59 +0400

    create main page

--- src/app/components/GetPopularPhotoes.tsx
+++ src/app/components/GetPopularPhotoes.tsx
@@ -1,11 +1,9 @@
-// This is your main Home page in Next.js
-
-// Fetch 20 most popular images from Unsplash
+import Image from "next/image";
+import { Photo } from "../types";
 async function getPopularPhotos() {
   const res = await fetch(
     `https://api.unsplash.com/photos?per_page=20&order_by=popular&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}`,
     {
-      // disables Next.js fetch caching (good for dev)
       cache: "no-store",
     }
   );
@@ -17,21 +15,22 @@ async function getPopularPhotos() {
   return res.json();
 }
 
-// The default exported Home component
-export default async function Photoes() {
-  const photos = await getPopularPhotos();
+export default async function GetpopularPhotoes() {
+  const photoes = await getPopularPhotos();
 
   return (
     <main className="p-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {photos.map((photo: any) => (
+      {photoes.map((photo: Photo) => (
         <div
           key={photo.id}
           className="overflow-hidden rounded-xl shadow-md bg-white"
         >
-          <img
+          <Image
             src={photo.urls.small}
             alt={photo.alt_description || "Unsplash image"}
-            className="w-full h-60 object-cover hover:scale-105 transition-transform duration-300"
+            width={400}
+            height={240}
+            className="w-full h-60 object-cover hover:scale-105 transition-transform duration-300 cursor-pointer"
           />
         </div>
       ))}
---
+++ src/app/types.tsx
@@ -0,0 +1,6 @@
+export interface Photo {
+  id: string;
+  urls: { small: string; regular: string };
+  alt_description: string;
+  user: { name: string };
+}
